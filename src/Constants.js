6b100f90e31639104bbd7c4fa08143f2dac50833
Yacine <yacineizegrouren@gmail.com>
2022-06-03 21:02:26 +0200

    add categorys

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,6 @@
-//export const BASE_URL_API= "http://localhost:8082/api";
+//export const BASE_URL_API= "http://localhost:8080/api";
+//export const BASE_URL_LOGIN = 'http://localhost:8080/login'
+
+
 export const BASE_URL_API= "https://api-cda-2022.herokuapp.com/api";
-export const BASE_URL_LOGIN= "https://api-cda-2022.herokuapp.com/login";
-//export const BASE_URL_LOGIN = 'http://localhost:8082/login'
+export const BASE_URL_LOGIN= "httpsnpm start://api-cda-2022.herokuapp.com/login";
