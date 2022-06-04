303511ce4808fc2cb6be17451aa792caa7f74b86
Yacine <yacineizegrouren@gmail.com>
2022-06-04 16:11:10 +0200

    add .env$

--- src/Constants.js
+++ src/Constants.js
@@ -2,5 +2,8 @@
 //export const BASE_URL_LOGIN = 'http://localhost:8080/login'
 
 
-export const BASE_URL_API= "https://api-cda-2022.herokuapp.com/api";
-export const BASE_URL_LOGIN= "httpsnpm start://api-cda-2022.herokuapp.com/login";
+//export const BASE_URL_API= "https://api-cda-2022.herokuapp.com/api";
+//export const BASE_URL_LOGIN= "httpsnpm start://api-cda-2022.herokuapp.com/login";
+
+export const BASE_URL_API= process.env.REACT_APP_API+"/api";
+export const BASE_URL_LOGIN= process.env.REACT_APP_API+"/login";
